e2ca976088b8d6f4df06ee00611aee2cbc005ec6
Sayaib Sarkar <ssayaib@gmail.com>
2021-04-26 00:26:17 +0530

    update datas

---
+++ app.js
@@ -0,0 +1,28 @@
+
+$.get( "https://api.covid19india.org/data.json", function( data ) {
+    $( ".result" ).html( data );
+    console.log(data['statewise'].length);   
+    
+    var tbval = document.getElementById('tbval');
+
+    for(var i=1; i<(data['statewise'].length); i++){
+        var x = tbval.insertRow();
+
+        x.insertCell(0);
+        tbval.rows[i].cells[0].innerHTML = data['statewise'][i-1]['state'];
+        x.insertCell(1);
+        tbval.rows[i].cells[1].innerHTML = data['statewise'][i-1]['active'];
+        x.insertCell(2);
+        tbval.rows[i].cells[2].innerHTML = data['statewise'][i-1]['confirmed'];
+        x.insertCell(3);
+        tbval.rows[i].cells[3].innerHTML = data['statewise'][i-1]['recovered'];
+        x.insertCell(4);
+        tbval.rows[i].cells[4].innerHTML = data['statewise'][i-1]['deaths'];
+        x.insertCell(5);
+        tbval.rows[i].cells[5].innerHTML = data['statewise'][i-1]['deltaconfirmed'];
+        x.insertCell(6);
+        tbval.rows[i].cells[6].innerHTML = data['statewise'][i-1]['deltadeaths'];
+        x.insertCell(7);
+        tbval.rows[i].cells[7].innerHTML = data['statewise'][i-1]['deltarecovered'];
+    }
+  });
